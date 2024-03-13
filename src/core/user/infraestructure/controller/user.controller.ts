10b9ac497c509517351cee110c7f9f677675f304
jorgepeniaranda <peniarandajorge@gmail.com>
2024-03-13 00:35:18 -0300

    feat: validate optional params

--- src/core/user/infraestructure/controller/user.controller.ts
+++ src/core/user/infraestructure/controller/user.controller.ts
@@ -2,6 +2,8 @@ import { type Request, type Response } from 'express'
 import { type UserUseCase } from '../../application/userUseCase'
 import { UserPhoneNumber } from '../../domain/valueObjects/phoneNumber/phoneNumber.value'
 import { ID } from '../../../../valueObjects/id/id.value'
+import { UserDNI } from '../../domain/valueObjects/dni/dni.value'
+import { UserEmail } from '../../domain/valueObjects/email/email.value'
 
 export default class UserController {
   constructor (private readonly userUseCase: UserUseCase) {}
@@ -10,12 +12,18 @@ export default class UserController {
     request: Request,
     response: Response
   ): Promise<Response> => {
-    const { id, phoneNumber } = request.query
+    const { id, dni, email, phoneNumber } = request.query
 
     const user = await this.userUseCase.findUser({
-      id: new ID(Number(id)),
-      phoneNumber: new UserPhoneNumber(Number(phoneNumber))
+      id: typeof id === 'string' ? new ID(Number(id)) : undefined,
+      dni: typeof dni === 'string' ? new UserDNI(Number(dni)) : undefined,
+      email: typeof email === 'string' ? new UserEmail(email) : undefined,
+      phoneNumber: typeof phoneNumber === 'string' ? new UserPhoneNumber(Number(phoneNumber)) : undefined
     })
+
+    if (user === null) {
+      return response.status(404).send()
+    }
 
     return response.json(user?.email).status(200)
   }
