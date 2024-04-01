7df45a881445e83c400b9f410aa38d6e33055b20
jorgepeniaranda <peniarandajorge@gmail.com>
2024-04-01 17:07:05 -0300

    feat: create function to retrieve secure environment values

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,5 +1,19 @@
 import { type INestApplication, Logger } from '@nestjs/common'
 
+export const getEnvValue = (envName: string, defaultValue?: string): string => {
+  const envValue = process.env[envName]
+
+  if (typeof envValue === 'string' && envValue.length > 0) {
+    return envValue
+  }
+
+  if (typeof defaultValue === 'string') {
+    return defaultValue
+  }
+
+  throw new Error('env error config: ' + envName)
+}
+
 export const findAvailablePort = async (server: INestApplication, port: number): Promise<number> => {
   server.listen(port).catch(async (error) => {
     if (error.code === 'EADDRINUSE') {
