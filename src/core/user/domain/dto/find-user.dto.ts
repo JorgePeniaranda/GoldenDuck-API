7eaeac7805ab187897d60c220a8d0b5ea3b08165
jorgepeniaranda <peniarandajorge@gmail.com>
2024-04-15 10:33:28 -0300

    fix: format document

--- src/core/user/domain/dto/find-user.dto.ts
+++ src/core/user/domain/dto/find-user.dto.ts
@@ -2,4 +2,6 @@ import { InputType, PartialType, PickType } from '@nestjs/graphql'
 import { UserDTO } from '../user.dto'
 
 @InputType()
-export class FindUserDTO extends PartialType(PickType(UserDTO, ['dni', 'email', 'phoneNumber'] as const)) {}
+export class FindUserDTO extends PartialType(
+  PickType(UserDTO, ['dni', 'email', 'phoneNumber'] as const)
+) {}
